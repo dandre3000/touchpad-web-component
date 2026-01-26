56e66e090a4e51196f55d919a8c5cb7ba2c7aa91
dandre3000 <deaundre.payne@gmail.com>
2026-01-26 04:34:52 -0500

    github pages

--- server.ts
+++ server.ts
@@ -2,8 +2,8 @@ const server = Bun.serve({
     // `routes` requires Bun v1.2.3+
     routes: {
         // Serve a file by lazily loading it into memory
-        "/": Bun.file("./index.html"),
-        "/build/touchpad.js": Bun.file(new URL("./build/touchpad.js", import.meta.url), { type: 'text/javascript' })
+        "/": Bun.file("./www/index.html"),
+        "/node_modules/@dandre3000/touchpad-web-component/build/touchpad.js": Bun.file(new URL("./build/touchpad.js", import.meta.url), { type: 'text/javascript' })
     }
 })
 
